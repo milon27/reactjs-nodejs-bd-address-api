5dbae0f06b4696f8853fec083632138f6a591030
milon27 <mdjahidulislammilon@gmail.com>
2020-07-20 00:04:36 +0600

    update db config file

--- db/config.js
+++ db/config.js
@@ -10,13 +10,14 @@ require("firebase/firestore");
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    databaseURL: process.env.databaseURL,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId,
+    apiKey: `${process.env.apiKey}`,
+    authDomain: `${process.env.authDomain}`,
+    databaseURL: `${process.env.databaseURL}`,
+    projectId: `${process.env.projectId}`,
+    storageBucket: `${process.env.storageBucket}`,
+    messagingSenderId: `${process.env.messagingSenderId}`,
+    appId: `${process.env.appId}`,
+    measurementId: `${process.env.measurementId}`
 };
 
 // Initialize Firebase
